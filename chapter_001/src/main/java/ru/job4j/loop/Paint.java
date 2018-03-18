96d85aaafaa1ea2fc56cff0ff72776b2db4e4e76
Maxim Belyakov <(zloblya03215@mail.ru)>
2018-03-19 00:01:20 +0300

    4.5. Рефакторинг кода.

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,49 +1,29 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
 
 public class Paint {
     public String rightTriangle(int height) {
-        StringBuilder stringBuilder = new StringBuilder();
-        int weight = height;
-        for (int row = 0; row != height; row++) {
-            for (int column = 0; column != weight; column++) {
-                if (row >= column) {
-                    stringBuilder.append("^");
-                }
-                else {
-                    stringBuilder.append(" ");
-                }
-            }
-            stringBuilder.append(System.lineSeparator());
-        }
-        return stringBuilder.toString();
+        return this.loopBy(height, height, (row, column) -> row >= column);
     }
 
     public String leftTriangle(int height) {
-        StringBuilder stringBuilder = new StringBuilder();
-        int weight = height;
-        for (int row = 0; row != height; row++) {
-            for (int column = 0; column != weight; column++) {
-                if (row >= weight - column - 1) {
-                    stringBuilder.append("^");
-                }
-                else {
-                    stringBuilder.append(" ");
-                }
-            }
-            stringBuilder.append(System.lineSeparator());
-        }
-        return stringBuilder.toString();
+        return this.loopBy(height, height, (row, column) -> row >= height - column - 1);
     }
 
     public String allTriangle(int height) {
+        return this.loopBy(height, 2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column);
+    }
+
+    private String loopBy(int height, int weight, BiPredicate<Integer, Integer> predicate) {
         StringBuilder stringBuilder = new StringBuilder();
-        int weight = 2 * height - 1;
         for (int row = 0; row != height; row++) {
             for (int column = 0; column != weight; column++) {
-                if (row >= height - column - 1 && row + height - 1 >= column) {
+                if (predicate.test(row, column)) {
                     stringBuilder.append("^");
-                } else {
+                }
+                else {
                     stringBuilder.append(" ");
                 }
             }
