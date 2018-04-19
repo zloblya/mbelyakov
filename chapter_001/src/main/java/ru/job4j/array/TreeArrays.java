13153c8ec6650cfb0480886c0dcfed2fd588f4dc
Maxim Belyakov <(zloblya03215@mail.ru)>
2018-04-19 23:46:54 +0300

    Дополнительное задание от Петра (отсортировать два массива)

--- chapter_001/src/main/java/ru/job4j/array/TreeArrays.java
+++ chapter_001/src/main/java/ru/job4j/array/TreeArrays.java
@@ -1,23 +1,13 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+
 public class TreeArrays {
     public int[] sortingArrays(int[] oneInput, int[] twoInput) {
         int[] array = new int[oneInput.length + twoInput.length];
-        int result;
-        int arrayOne;
-        int arrayTwo;
-        for (result = arrayOne = arrayTwo = 0; result < array.length; result++) {
-            if (arrayOne < oneInput.length)
-                if (arrayTwo < twoInput.length)
-                    if (oneInput[arrayOne] < twoInput[arrayTwo])
-                        array[result] = oneInput[arrayOne++];
-                    else
-                        array[result] = twoInput[arrayTwo++];
-                else
-                    array[result] = oneInput[arrayOne++];
-            else
-                array[result] = twoInput[arrayTwo];
-        }
+        System.arraycopy(oneInput, 0, array, 0, oneInput.length);
+        System.arraycopy(twoInput, 0, array, oneInput.length, twoInput.length);
+        Arrays.sort(array);
         return array;
     }
 }
