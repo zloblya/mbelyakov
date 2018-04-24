b9f6d2ee328b74d8a0950b6b94b87f1616556c1d
Maxim Belyakov <(zloblya03215@mail.ru)>
2018-04-24 23:08:11 +0300

    Дополнительное задание от Петра (отсортировать два массива) без комментариев к коду

--- chapter_001/src/main/java/ru/job4j/array/TreeArrays.java
+++ chapter_001/src/main/java/ru/job4j/array/TreeArrays.java
@@ -1,13 +1,18 @@
 package ru.job4j.array;
 
-import java.util.Arrays;
-
 public class TreeArrays {
     public int[] sortingArrays(int[] oneInput, int[] twoInput) {
-        int[] array = new int[oneInput.length + twoInput.length];
-        System.arraycopy(oneInput, 0, array, 0, oneInput.length);
-        System.arraycopy(twoInput, 0, array, oneInput.length, twoInput.length);
-        Arrays.sort(array);
-        return array;
+        int[] three = new int[oneInput.length + twoInput.length];
+        int i = 0, j = 0, index = 0;
+        while (i < oneInput.length && j < twoInput.length) {
+            three[index++] = oneInput[i] < twoInput[j] ? oneInput[i++] :  twoInput[j++];
+        }
+        if (i < oneInput.length) {
+            System.arraycopy(oneInput, i, three, index, (oneInput.length - i));
+        }
+        if (j < twoInput.length) {
+            System.arraycopy(twoInput, j, three, index, (twoInput.length - j));
+        }
+        return three;
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/TreeArraysTest.java
+++ chapter_001/src/test/java/ru/job4j/array/TreeArraysTest.java
@@ -10,8 +10,8 @@ public class TreeArraysTest {
     @Test
     public void sortingArrays() {
         TreeArrays threeArrays = new TreeArrays();
-        int[] result = threeArrays.sortingArrays(new int[]{1,3,13,27,50,7,60}, new int[]{2,4,5,90});
-        int[] expected = new int[] {1,2,3,4,5,7,13,27,50,60,90};
+        int[] result = threeArrays.sortingArrays(new int[]{1,3,13,27,50,60}, new int[]{2,4,5,90});
+        int[] expected = new int[] {1,2,3,4,5,13,27,50,60,90};
         assertThat(result, is(expected));
     }
 }
